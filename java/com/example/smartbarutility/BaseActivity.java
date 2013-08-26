3c0821448cfc6ac0399ccb05aebbb001780babd5
calmer <calmer91@gmail.com>
2013-08-26 13:31:28 +0800

    Add sample code

--- java/com/example/smartbarutility/BaseActivity.java
+++ java/com/example/smartbarutility/BaseActivity.java
@@ -12,7 +12,16 @@ public class BaseActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(SmartBarUtils.isMX2()){
+            //方式1,推荐
             SmartBarUtils.hide(this, getWindow(),SmartBarUtils.SMART_BAR_HEIGHT_PIXEL);
+
+//            //方式2
+//            requestWindowFeature(Window.FEATURE_NO_TITLE);
+//            SmartBarUtils.hide(getWindow().getDecorView());
+//
+//            //方式3
+//            requestWindowFeature(Window.FEATURE_NO_TITLE);
+//            SmartBarUtils.hide(this, getWindow());
         }
         setContentView(R.layout.activity_base);
     }
